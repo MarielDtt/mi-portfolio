66659c3f73a568bd15920073b8087df20ac5d7e0
MarielDtt <marieldruetta@gmail.com>
2025-03-24 19:09:48 -0300

    Fondos section

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,20 +4,24 @@ function Home() {
 
     return (
 
-        <div className="w-[90%]">
-            <section className="bg-amber-200 w-full h-96" id="inicio">
+        <div className="w-[90%] mt-4">
+            <section className="bg-radial-[at_50%_75%] from-[#D7F4F7] via-[#F8E0D7] to-[#FDE8D7] to-90%
+             w-full h-96 rounded-xl" id="inicio">
                 Inicio
             </section>
-            <section className="bg-amber-700 w-full h-96" id="proyectos">
+            <section className="bg-radial-[at_50%_75%] from-[#FDE8D7] via-[#F8E0D7] to-[#D7F4F7] to-90%
+             w-full h-96 mt-4 rounded-xl" id="proyectos">
                 Proyectos
             </section>
-            <section className="bg-amber-900 w-full h-96" id="tecnologias">
+            <section className="bg-radial-[at_50%_75%] from-[#FDE8D7] via-[#FFE3F3] to-[#D7F4F7] to-90%
+             w-full h-96 mt-4 rounded-xl" id="tecnologias">
                 Tecnologías
             </section>
-            <section className="bg-blue-600 w-full h-96" id="sobremi">
+            <section className="bg-radial-[at_50%_75%] from-[#D7F4F7] via-[#E6F4FA] to-[#FDE8D7] to-90%
+            w-full h-96 mt-4 rounded-xl" id="sobremi">
                 Sobre mí
             </section>
-            <section className="bg-pink-300 w-full h-96" id="contacto">
+            <section className="bg-radial-[at_50%_75%] from-[#FDE8D7] via-[#ECE1F9] to-[#D7F4F7] to-90% w-full h-96 mt-4 mb-4 rounded-xl" id="contacto">
                 Contacto
             </section>
         </div>
