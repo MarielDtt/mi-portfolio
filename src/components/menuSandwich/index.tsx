fc7af84f03567262a244a706fca30315885f50b2
MarielDtt <marieldruetta@gmail.com>
2025-03-24 18:04:06 -0300

    menu desplegable se cierra al scrolear

--- src/components/menuSandwich/index.tsx
+++ src/components/menuSandwich/index.tsx
@@ -3,67 +3,86 @@ import {
     MenuHandler,
     MenuList,
     MenuItem,
-
 } from "@material-tailwind/react";
 
-import { SquareMenu } from 'lucide-react';
-
+import { SquareMenu } from "lucide-react";
+import { useState, useEffect } from "react";
 
 export function MenuSandwich() {
+
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (open) {
+                setOpen(false);
+            }
+        };
+    
+        window.addEventListener("scroll", handleScroll);
+    
+        // Limpiar el evento al desmontar
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [open]); 
+
     return (
-        <Menu>
+        <Menu
+            placement="bottom-end"
+            open={open}
+            handler={setOpen}
+        >
             <MenuHandler>
-                <div>
+                <div className="cursor-pointer">
                     <SquareMenu />
                 </div>
             </MenuHandler>
             <MenuList
-                className="absolute w-42 p-2 rounded-xl bg-white/20 backdrop-blur-md shadow-lg z-50 border border-black"
+                className="w-48 p-2 rounded-xl bg-white/20 backdrop-blur-md shadow-lg z-50 border border-black"
                 placeholder=""
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
+                onPointerEnterCapture={() => { }}
+                onPointerLeaveCapture={() => { }}
             >
                 <MenuItem
-                    className="border-b border-black last:border-none"
+                    className="border-b border-black last:border-none hover:bg-white/30 transition-colors duration-200 rounded-md"
                     placeholder=""
-                    onPointerEnterCapture=""
-                    onPointerLeaveCapture=""
+                    onPointerEnterCapture={() => { }}
+                    onPointerLeaveCapture={() => { }}
                 >
                     Inicio
                 </MenuItem>
                 <MenuItem
-                    className="border-b border-black last:border-none"
+                    className="border-b border-black last:border-none hover:bg-white/30 transition-colors duration-200 rounded-md"
                     placeholder=""
-                    onPointerEnterCapture=""
-                    onPointerLeaveCapture=""
+                    onPointerEnterCapture={() => { }}
+                    onPointerLeaveCapture={() => { }}
                 >
                     Proyectos
                 </MenuItem>
                 <MenuItem
-                    className="border-b border-black last:border-none"
+                    className="border-b border-black last:border-none hover:bg-white/30 transition-colors duration-200 rounded-md"
                     placeholder=""
-                    onPointerEnterCapture=""
-                    onPointerLeaveCapture=""
+                    onPointerEnterCapture={() => { }}
+                    onPointerLeaveCapture={() => { }}
                 >
                     Tecnologías
                 </MenuItem>
-                <MenuItem
-                    className="border-b border-black last:border-none"
+                <MenuItem className="border-b border-black last:border-none hover:bg-white/30 transition-colors duration-200 rounded-md"
                     placeholder=""
-                    onPointerEnterCapture=""
-                    onPointerLeaveCapture=""
+                    onPointerEnterCapture={() => { }}
+                    onPointerLeaveCapture={() => { }}
                 >
                     Sobre mí
                 </MenuItem>
-                <MenuItem
-                    className="border-b border-black last:border-none"
+                <MenuItem className="border-b border-black last:border-none hover:bg-white/30 transition-colors duration-200 rounded-md"
                     placeholder=""
-                    onPointerEnterCapture=""
-                    onPointerLeaveCapture=""
+                    onPointerEnterCapture={() => { }}
+                    onPointerLeaveCapture={() => { }}
                 >
                     Contacto
                 </MenuItem>
             </MenuList>
-        </Menu>
+        </Menu >
     );
 }
