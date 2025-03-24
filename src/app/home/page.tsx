ea5be6093a255dcca0169f7485e7fddc86dd8ac9
SistemagestionCem <sistemagestion.cem@gmail.com>
2025-03-23 23:02:32 -0300

    bosquejo de section

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,17 +4,23 @@ function Home() {
 
     return (
 
-        <>
-        <div className="bg-amber-200 w-1/2 h-96">
-            probando
+        <div className="w-[90%]">
+            <section className="bg-amber-200 w-full h-96" id="inicio">
+                Inicio
+            </section>
+            <section className="bg-amber-700 w-full h-96" id="proyectos">
+                Proyectos
+            </section>
+            <section className="bg-amber-900 w-full h-96" id="tecnologias">
+                Tecnologías
+            </section>
+            <section className="bg-blue-600 w-full h-96" id="sobremi">
+                Sobre mí
+            </section>
+            <section className="bg-pink-300 w-full h-96" id="contacto">
+                Contacto
+            </section>
         </div>
-        <div className="bg-amber-700 w-1/2 h-96">
-            probando
-        </div>
-        <div className="bg-amber-900 w-1/2 h-96">
-            probando
-        </div>
-        </>
 
     )
 }
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,15 +5,31 @@ function Navbar() {
 
     return (
         <nav className="w-[90%] h-16 border-white/20 sticky top-2 z-50 bg-white/10 backdrop-blur-lg rounded-xl mt-4 mx-auto shadow-md px-6 py-3">
-            <div className="pl-4">
-                <Image src="/Logo.png" alt="logo"  width={48} height={48}/>
-            </div>
-            <div className="flex">
-                <ul>Inicio</ul>
-                <ul>Proyectos</ul>
-                <ul>Tecnologías</ul>
-                <ul>Sobre mí</ul>
-                <ul>Contacto</ul>
+            <div className="flex justify-between w-full">
+                <div>
+                    <a href="#inicio" className="hover:text-white">
+                        <Image src="/Logo.png" alt="logo" width={48} height={48} />
+                    </a>
+                </div>
+                <div className="flex">
+                    <ul className="flex gap-4 font-semibold self-center ">
+                        <li>
+                            <a href="#inicio" className="hover:text-white">Inicio</a>
+                        </li>
+                        <li>
+                            <a href="#proyectos" className="hover:text-white">Proyectos</a>
+                        </li>
+                        <li>
+                            <a href="#tecnologias" className="hover:text-white">Tecnologías</a>
+                        </li>
+                        <li>
+                            <a href="#sobremi" className="hover:text-white">Sobre mí</a>
+                        </li>
+                        <li>
+                            <a href="#contacto" className="hover:text-white">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
