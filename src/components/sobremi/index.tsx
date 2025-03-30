0fc7a7a4b6dd356ad05eb655da0e2acef1178610
MarielDtt <marieldruetta@gmail.com>
2025-03-30 03:01:11 -0300

    Comienzo de estilos a la section Sobre Mi

--- src/components/sobremi/index.tsx
+++ src/components/sobremi/index.tsx
@@ -6,24 +6,36 @@ function SobreMi() {
         <>
             <h1 className="font-lilita-custom text-center pt-4 md:text-4xl text-2xl">¿Y quién lanza las pelotitas? Yo </h1>
 
-            <div className="pl-4 mt-4">
+            <div className="flex pl-4 mt-4">
                 <Image src="/perfil.jpg"
                     alt="foto perfil"
                     width={0}
                     height={0}
                     sizes="100vw"
-                    className="w-40 h-40 md:w-64 md:h-64 rounded-full"
+                    className="hidden md:block w-64 h-64 rounded-full"
+                />
+                <div>
+                    <p className="font-poppins-custom mx-8 mt-4 text-base md:text-lg leading-relaxed text-justify">
+                        Soy Mariel, desarrolladora web con formación en Frontend y una pasión enorme por crear experiencias únicas.
+                        Aprendí a lanzar pelotitas de código en distintos proyectos, y cada una fue tomando forma: diseño, funcionalidad, interacción.
+                        Vengo de otro mundo profesional, pero encontré en la tecnología un espacio donde mi creatividad puede explotar (¡con glitter pastel, por supuesto!).
+
+                    </p>
+
+                    <p className="font-poppins-custom mx-8 mt-4 text-base md:text-lg leading-relaxed text-justify">
+                        Me encantan los gatos, tengo dos: Galileo Galilei y Davinci, y soy muy fan de Los Simpsons.
+                        Me entusiasma resolver problemas, trabajar en equipo y seguir aprendiendo.
+                        ¿Vamos a lanzar algunas ideas juntos?
+                    </p>
+                </div>
+                <Image src="/perfil.jpg"
+                    alt="foto perfil"
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    className="block md:hidden mx-auto w-40 h-40 rounded-full"
                 />
             </div>
-            <p>Soy Mariel, desarrolladora web con formación en Frontend y una pasión enorme por crear experiencias únicas.
-                Aprendí a lanzar pelotitas de código en distintos proyectos, y cada una fue tomando forma: diseño, funcionalidad, interacción.
-                Vengo de otro mundo profesional, pero encontré en la tecnología un espacio donde mi creatividad puede explotar (¡con glitter pastel, por supuesto!).
-
-                Me encantan los gatos —tengo dos: Galileo Galilei y Davinci— y soy muy fan de Los Simpsons.
-                Me entusiasma resolver problemas, trabajar en equipo y seguir aprendiendo.
-                ¿Vamos a lanzar algunas ideas juntos?</p>
-            <Image src="/gatos.jpeg" alt="foto gatos" width={72} height={72} />
-            <Image src="/simpsons.webp" alt="foto simpsons" width={72} height={72} />
         </>
     )
 }
