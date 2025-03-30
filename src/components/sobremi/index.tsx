3a73629109c78ae932aefce2689ed0174a6a3d74
MarielDtt <marieldruetta@gmail.com>
2025-03-30 20:30:53 -0300

    Las imagenes han  sido implementada

--- src/components/sobremi/index.tsx
+++ src/components/sobremi/index.tsx
@@ -1,20 +1,40 @@
+"use client"
 import Image from "next/image";
+import { useState } from "react";
 
+
+const imagenes: string[] = [
+    "/perfil.jpg",
+    "/simpsons.png",
+    "/lapiz.png",
+    "/manga.png",
+    "/ghibli.png"
+]
 function SobreMi() {
+    const [imagenEnPantalla, setImagenEnPantalla] = useState(0)
 
+    const handleImagenClick = () => {
+
+        setImagenEnPantalla((prev) =>
+            prev === imagenes.length - 1 ? 0 : prev + 1
+        );
+    }
     return (
         <>
             <h1 className="font-lilita-custom text-center pt-4 md:text-4xl text-2xl">¿Y quién lanza las pelotitas? Yo </h1>
 
-            <div className="flex pl-4 mt-4 flex-col md:flex-row cursor-pointer">
+            <div
+                className="flex pl-4 mt-4 flex-col md:flex-row cursor-pointer"
+                onClick={handleImagenClick}>
 
-                <div className="relative group rounded-full w-4/5 h-64 hidden md:block shadow-[0_0_15px_rgba(255,182,193,0.4)] hover:shadow-[0_0_25px_rgba(255,182,193,0.8)] transition-shadow duration-300">
-                    <Image src="/perfil.jpg"
+                <div className="relative group rounded-full w-4/5 h-64 hidden md:block shadow-[0_0_15px_rgba(255,182,193,0.4)] hover:shadow-[0_0_25px_rgba(255,182,193,0.8)] transition-shadow duration-300" >
+                    <Image src={imagenes[imagenEnPantalla]}
                         alt="foto perfil"
                         width={0}
                         height={0}
                         sizes="100vw"
                         className=" w-full h-full rounded-full"
+
                     />
                     <Image
                         src="/brillo-uno.png"
@@ -24,22 +44,22 @@ function SobreMi() {
                         className="absolute top-0 left-0  w-24 h-24 animate-pulse pointer-events-none"
                     />
                     <div className="opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out">
-                    <Image
-                        src="/brillo-dos.png"
-                        alt="brillos decorativos"
-                        width={92}
-                        height={92}
-                        className="absolute bottom-2 left-2  w-18 h-18 animate-pulse pointer-events-none"
-                    />
-                     <Image
-                        src="/brillo-tres.png"
-                        alt="brillos decorativos"
-                        width={92}
-                        height={92}
-                        className="absolute top-20 right-0  w-16 h-16 animate-pulse pointer-events-none"
-                    />
+                        <Image
+                            src="/brillo-dos.png"
+                            alt="brillos decorativos"
+                            width={92}
+                            height={92}
+                            className="absolute bottom-2 left-2  w-18 h-18 animate-pulse pointer-events-none"
+                        />
+                        <Image
+                            src="/brillo-tres.png"
+                            alt="brillos decorativos"
+                            width={92}
+                            height={92}
+                            className="absolute top-20 right-0  w-16 h-16 animate-pulse pointer-events-none"
+                        />
                     </div>
-                   
+
                 </div>
                 <div className="rounded-full w-1/3 h-36 md:hidden mx-auto shadow-[0_0_15px_rgba(255,182,193,0.4)] hover:shadow-[0_0_25px_rgba(255,182,193,0.8)] transition-shadow duration-300 relative">
                     <Image src="/perfil.jpg"
