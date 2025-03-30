bf625062829bec177f37fdcc934c7021117b6336
MarielDtt <marieldruetta@gmail.com>
2025-03-30 03:40:00 -0300

    Section Sobre Mi estilos completos FALTAN EFECTOS

--- src/components/sobremi/index.tsx
+++ src/components/sobremi/index.tsx
@@ -6,7 +6,7 @@ function SobreMi() {
         <>
             <h1 className="font-lilita-custom text-center pt-4 md:text-4xl text-2xl">¿Y quién lanza las pelotitas? Yo </h1>
 
-            <div className="flex pl-4 mt-4">
+            <div className="flex pl-4 mt-4 flex-col md:flex-row">
                 <Image src="/perfil.jpg"
                     alt="foto perfil"
                     width={0}
@@ -14,7 +14,15 @@ function SobreMi() {
                     sizes="100vw"
                     className="hidden md:block w-64 h-64 rounded-full"
                 />
-                <div>
+
+                <Image src="/perfil.jpg"
+                    alt="foto perfil"
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    className="block md:hidden mx-auto w-40 h-40 rounded-full"
+                />
+                <div className="overflow-y-auto md:overflow-visible">
                     <p className="font-poppins-custom mx-8 mt-4 text-base md:text-lg leading-relaxed text-justify">
                         Soy Mariel, desarrolladora web con formación en Frontend y una pasión enorme por crear experiencias únicas.
                         Aprendí a lanzar pelotitas de código en distintos proyectos, y cada una fue tomando forma: diseño, funcionalidad, interacción.
@@ -25,16 +33,13 @@ function SobreMi() {
                     <p className="font-poppins-custom mx-8 mt-4 text-base md:text-lg leading-relaxed text-justify">
                         Me encantan los gatos, tengo dos: Galileo Galilei y Davinci, y soy muy fan de Los Simpsons.
                         Me entusiasma resolver problemas, trabajar en equipo y seguir aprendiendo.
+                    </p>
+                    <p className="font-poppins-custom mx-8 mt-4 text-base md:text-lg leading-relaxed text-justify">
                         ¿Vamos a lanzar algunas ideas juntos?
                     </p>
+
                 </div>
-                <Image src="/perfil.jpg"
-                    alt="foto perfil"
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    className="block md:hidden mx-auto w-40 h-40 rounded-full"
-                />
+
             </div>
         </>
     )
