c1c2354252c5fb4bda78cefcad99207339c68fd1
MarielDtt <marieldruetta@gmail.com>
2025-04-01 00:49:14 -0300

    Incorporamos Tecnologias

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -10,22 +10,22 @@ function Home() {
 
         <div className="w-[90%] mt-4">
             <section className="bg-radial-[at_50%_75%] from-[#D7F4F7] via-[#F8E0D7] to-[#FDE8D7] to-90%
-             w-full h-96 rounded-xl" id="inicio">
+             w-full h-96 rounded-xl mb-16" id="inicio">
                 <Inicio/>
             </section>
             <section className="bg-radial-[at_50%_75%] from-[#FDE8D7] via-[#F8E0D7] to-[#D7F4F7] to-90%
-             w-full h-96 mt-4 rounded-xl" id="proyectos">
+             w-full h-96 mt-4 rounded-xl mb-16" id="proyectos">
                 <Proyectos/>
             </section>
             <section className="bg-radial-[at_50%_75%] from-[#FDE8D7] via-[#FFE3F3] to-[#D7F4F7] to-90%
-             w-full h-96 mt-4 rounded-xl" id="tecnologias">
+             w-full h-96 mt-4 rounded-xl mb-16" id="tecnologias">
                 <Tecnologias/>
             </section>
             <section className="bg-radial-[at_50%_75%] from-[#D7F4F7] via-[#E6F4FA] to-[#FDE8D7] to-90%
-            w-full md:h-96 h-auto mt-4 rounded-xl pb-4" id="sobremi">
+            w-full md:h-96 h-auto mt-4 rounded-xl mb-16" id="sobremi">
                <SobreMi/>
             </section>
-            <section className="bg-radial-[at_50%_75%] from-[#FDE8D7] via-[#ECE1F9] to-[#D7F4F7] to-90%gi w-full h-96 mt-4 mb-4 rounded-xl" id="contacto">
+            <section className="bg-radial-[at_50%_75%] from-[#FDE8D7] via-[#ECE1F9] to-[#D7F4F7] to-90%gi w-full h-96 mt-4 rounded-xl mb-16" id="contacto">
                 <Contacto/>
             </section>
         </div>
--- src/components/tecnologias/index.tsx
+++ src/components/tecnologias/index.tsx
@@ -1,20 +1,61 @@
 import Image from "next/image";
+type ImagenTecnologia = {
+    id: number;
+    img: string;
+};
+
+const imagenes: ImagenTecnologia[] = [
+    {
+        id: 1,
+        img: "/react.png"
+    },
+    {
+        id: 2,
+        img: "/mongo.png"
+    },
+    {
+        id: 3,
+        img: "/next.png"
+    },
+    {
+        id: 4,
+        img: "/js.png"
+    },
+    {
+        id: 5,
+        img: "/node-express.png"
+    },
+    {
+        id: 6,
+        img: "/postgreSQL.png"
+
+    },
+    {
+        id: 7,
+        img: "/tailwind.png"
+    },
+    {
+        id: 8,
+        img: "/ts.png"
+    }
+]
+
 function Tecnologias() {
 
     return (
         <>
-            <h1>Tecnologías que hacen la magia</h1>
-            <p>Ya aprendí a manejar este panel de tecnologías sin provocar un desastre nuclear ☢️
-                Pero ojo… ¡esto recién empieza!
-                Estoy constantemente sumando nuevas habilidades.</p>
-            <Image src="/js.png" alt="logo javaScrip" width={24} height={24} />
-            <Image src="/mongo.png" alt="logo mongo" width={24} height={24} />
-            <Image src="/next.png" alt="logo next" width={24} height={24} />
-            <Image src="/node-express.png" alt="logo node-express" width={24} height={24} />
-            <Image src="/postgreSQL.png" alt="logo postgreSQL" width={24} height={24} />
-            <Image src="/react.png" alt="logo react" width={24} height={24} />
-            <Image src="/tailwind.png" alt="logo tailwind" width={24} height={24} />
-            <Image src="/ts.png" alt="logo typeScript" width={24} height={24} />
+            <h1 className="font-lilita-custom text-center pt-4 md:text-4xl text-2xl">Tecnologías que hacen la magia</h1>
+            <p className="font-poppins-custom mx-8 mt-2 text-base md:text-lg leading-relaxed text-justify">Ya aprendí a manejar este panel de tecnologías sin provocar un desastre nuclear ☢️</p>
+            <p className="font-poppins-custom mx-8 mt-2 text-base md:text-lg leading-relaxed text-justify">Pero ojo… ¡esto recién empieza!</p>
+            <p className="font-poppins-custom mx-8 mt-2 text-base md:text-lg leading-relaxed text-justify">Estoy constantemente sumando nuevas habilidades.</p>
+
+            <div className="relative flex items-center mt-8">
+                <div id="Slider" className="mx-auto">
+                    {imagenes.map((item) => (
+                        <Image className="w-[100px] inline-block p-2 cursor-pointer hover:scale-150 ease-in-out duration-300" key={item.id} src={item.img} alt="/" width={100} height={64} />
+                    ))}
+                </div>
+            </div>
         </>
     )
 }
