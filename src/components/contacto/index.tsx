6ad746d94679cff1d189dbcb57f0d040bb5a93d6
MarielDtt <marieldruetta@gmail.com>
2025-03-31 21:28:20 -0300

    Bosquejo Icono seccion Contacto

--- src/components/contacto/index.tsx
+++ src/components/contacto/index.tsx
@@ -1,3 +1,4 @@
+import { Globe, Mail, MapPin, PhoneCall } from "lucide-react";
 
 function Contacto() {
 
@@ -8,27 +9,33 @@ function Contacto() {
             <h2 className="font-poppins-custom mx-8 mt-4 text-base md:text-lg leading-relaxed text-justify pb-4">Una chispa, una idea, una charla. Todo empieza con un mensaje.</h2>
 
             <div className="flex flex-col md:flex-row gap-8 pb-8 md:pb-12">
-                <div className="w-full md:w-1/2 space-y-4">
-
-                    <div className="flex">
-                        <p>Ciudad</p>
-
-                        <p>Email</p>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 text-center items-center w-full md:w-1/2 mx-auto">
+                    <div className="flex flex-col items-center">
+                        <MapPin size={48} color="#f7a35c" absoluteStrokeWidth />
+                        <p>Argentina</p>
+                        <p>C.A.B.A</p>
                     </div>
-
-                    <div>
-                        <p>Telefonos</p>
-                        <div>
-                            <p>Links Redes</p>
-                            <p>Hable mas Fuerte que tengo una toalla, o mejor hace click para escribirme</p>
-                        </div>
+                    <div className="flex flex-col items-center">
+                        <Mail size={48} color="#f7a35c" absoluteStrokeWidth />
+                        <p>Email</p>
+                        <p>marieldruetta@gmail.com</p>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <PhoneCall size={48} color="#f7a35c" absoluteStrokeWidth />
+                        <p>Teléfonos</p>
+                        <p>54-11-66603916</p>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <Globe size={48} color="#f7a35c" absoluteStrokeWidth />
+                        <p>Links Redes</p>
+                        <p>Hablá más fuerte que tengo una toalla, o mejor hacé click para escribirme</p>
                     </div>
                 </div>
-                
+
                 <div className="w-full md:w-1/2">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d105073.2747114119!2d-58.51587114103547!3d-34.615795932890286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca3b4ef90cbd%3A0xa0b3812e88e88e87!2sBuenos%20Aires%2C%20Cdad.%20Aut%C3%B3noma%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1743395541646!5m2!1ses!2sar"
-                        className="w-92 h-[250px] md:h-[250px] rounded-2xl shadow-lg mx-auto"
+                        className="w-[90%] h-[200px] md:h-[250px] rounded-2xl shadow-[0_0_25px_rgba(255,182,193,0.8)] mx-auto"
                         style={{ border: 0 }}
                         allowFullScreen
                         loading="lazy"
