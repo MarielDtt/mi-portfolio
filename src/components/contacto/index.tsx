fd702fde902263c140456dc9d94d23161f411b8b
MarielDtt <marieldruetta@gmail.com>
2025-03-31 22:26:48 -0300

    Agrego Responsive a Contacto

--- src/components/contacto/index.tsx
+++ src/components/contacto/index.tsx
@@ -10,8 +10,8 @@ function Contacto() {
 
             <h2 className="font-poppins-custom mx-8 mt-4 text-base md:text-lg leading-relaxed text-justify pb-4">Una chispa, una idea, una charla. Todo empieza con un mensaje.</h2>
 
-            <div className="flex flex-col md:flex-row gap-8 pb-8 md:pb-12">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 text-center items-center w-full md:w-1/2 mx-auto">
+            <div className="flex flex-col md:flex-row gap-8 pb-8 md:pb-12 ">
+                <div className="grid grid-cols-2 md:grid-cols-2 gap-6 text-center items-center w-full md:w-1/2 mx-auto ">
                     <div className="flex flex-col items-center">
                         <MapPin size={48} color="#f7a35c" absoluteStrokeWidth />
                         <p className="font-bold pt-2">Ubicacion</p>
@@ -44,7 +44,7 @@ function Contacto() {
                     </div>
                 </div>
 
-                <div className="w-full md:w-1/2">
+                <div className="w-full md:w-1/2 hidden md:block">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d105073.2747114119!2d-58.51587114103547!3d-34.615795932890286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca3b4ef90cbd%3A0xa0b3812e88e88e87!2sBuenos%20Aires%2C%20Cdad.%20Aut%C3%B3noma%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1743395541646!5m2!1ses!2sar"
                         className="w-[90%] h-[200px] md:h-[250px] rounded-2xl shadow-[0_0_25px_rgba(255,182,193,0.8)] mx-auto"
