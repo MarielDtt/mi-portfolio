9d5676ba10a605034b991afe36101f1b9ff332ed
MarielDtt <marieldruetta@gmail.com>
2025-04-02 00:57:12 -0300

    Animacion a Bajada de Inicio

--- src/components/inicio/index.tsx
+++ src/components/inicio/index.tsx
@@ -1,20 +1,34 @@
 "use client"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ButtonRounded } from "../buttons";
 import SkillsIcons from "../skillsIcons";
 
 const textoAnimado = ["Desarrollador FullStack", "Desarrollador Frontend", "Desarrollador Web"]
 function Inicio() {
 
-const [posicionFraseActual , setPosicionFraseActual ] = useState(0);
-const [characterByCharacter , setCharacterByCharacter ] = useState("")
+    const [posicionFraseActual, setPosicionFraseActual] = useState(0);
+    const [characterByCharacter, setCharacterByCharacter] = useState("")
+
+    useEffect(() => {
+     const timeoutId= setTimeout(() => {
+
+            if (posicionFraseActual < textoAnimado.length - 1) {
+                setPosicionFraseActual(posicionFraseActual + 1);
+            } else {
+                setPosicionFraseActual(0);
+            }
+        }, 1000); 
+        return () => clearTimeout(timeoutId)
+    }, [posicionFraseActual]);
+
+
     return (
         <div className="w-full h-96 flex flex-col pt-24 pl-8">
             <h1 className="font-lilita-custom md:text-6xl text-4xl">
                 MARIEL DRUETTA
             </h1>
             <div className="pt-8 ">
-                <h2 className="text-rose-500 font-extrabold text-2xl text-center w-2/5 special-elite-text">{textoAnimado[posicionFraseActual]}</h2>
+                <h2 key={posicionFraseActual} className="text-rose-500 text-2xl text-center w-2/5 special-elite-text">{textoAnimado[posicionFraseActual]}</h2>
             </div>
             <div className="w-2/5 pt-8 flex justify-center">
                 <ButtonRounded
